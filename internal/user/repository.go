0c0adb8ba5a94ce62f8fac28f42433f5b486e7de
Lucas Ribeiro <lucasribeiro292004@hotmail.com>
2026-04-14 16:28:42 -0300

    feat: update user repository and service to use context and return ent.User

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,10 +1,14 @@
 package user
 
-import "choseclothes/ent"
+import (
+	"context"
+
+	"choseclothes/ent"
+)
 
 type Repository interface {
-	GetUsers() []User
-	CreateUser(user User) error
+	GetUsers(ctx context.Context) ([]*ent.User, error)
+	CreateUser(ctx context.Context, u CreateUserInput) (*ent.User, error)
 }
 
 type repository struct {
@@ -17,12 +21,25 @@ func NewRepository(client *ent.Client) Repository {
 	}
 }
 
-func (r *repository) GetUsers() []User {
-
-	return nil
+func (r *repository) GetUsers(ctx context.Context) ([]*ent.User, error) {
+	return r.client.User.Query().All(ctx)
 }
 
-func (r *repository) CreateUser(user User) error {
+func (r *repository) CreateUser(ctx context.Context, u CreateUserInput) (*ent.User, error) {
+	return r.client.User.
+		Create().
+		SetNillableGoogleID(nillableString(u.GoogleId)).
+		SetEmail(u.Email).
+		SetNillablePasswordHash(nillableString(u.Password)).
+		SetNillableName(nillableString(u.Name)).
+		SetIsSubscriber(u.IsSubscriber).
+		Save(ctx)
+}
 
-	return nil
+// helper simples pra optional string
+func nillableString(s string) *string {
+	if s == "" {
+		return nil
+	}
+	return &s
 }
--- internal/user/service.go
+++ internal/user/service.go
@@ -1,5 +1,9 @@
 package user
 
+import (
+	"context"
+)
+
 type service struct {
 	repo Repository
 }
@@ -11,21 +15,42 @@ func NewService(repo Repository) Service {
 }
 
 type Service interface {
-	GetUsers() []User
-	CreateUser(user User) (User, error)
+	GetUsers(ctx context.Context) ([]User, error)
+	CreateUser(ctx context.Context, input CreateUserInput) (User, error)
 }
 
-func (s *service) GetUsers() []User {
-	return s.repo.GetUsers()
+func (s *service) GetUsers(ctx context.Context) ([]User, error) {
+	users, err := s.repo.GetUsers(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]User, 0, len(users))
+
+	for _, u := range users {
+		result = append(result, User{
+			Id:           u.ID,
+			Name:         u.Name,
+			Email:        u.Email,
+			GoogleId:     u.GoogleID,
+			IsSubscriber: u.IsSubscriber,
+		})
+	}
+
+	return result, nil
 }
 
-func (s *service) CreateUser(user User) (User, error) {
-
-
-	err := s.repo.CreateUser(user)
+func (s *service) CreateUser(ctx context.Context, input CreateUserInput) (User, error) {
+	u, err := s.repo.CreateUser(ctx, input)
 	if err != nil {
 		return User{}, err
 	}
 
-	return user, nil
+	return User{
+		Id:           u.ID,
+		Name:         u.Name,
+		Email:        u.Email,
+		GoogleId:     u.GoogleID,
+		IsSubscriber: u.IsSubscriber,
+	}, nil
 }
